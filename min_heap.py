0a487fa4da6a80ac740c9843deb8aa0f48cee83d
Ethan Saxenian <esaxenian@middlebury.edu>
2021-12-18 23:12:32 -0500

    heapify

--- min_heap.py
+++ min_heap.py
@@ -1,9 +1,19 @@
 from math import floor
+import random
 
 
 class MinHeap:
     def __init__(self):
         self.array = []
+
+    def __repr__(self):
+        return f"MinHeap({self.array})"
+
+    def __bool__(self):
+        return bool(self.array)
+
+    def __len__(self):
+        return len(self.array)
 
     @staticmethod
     def _lchild_index(i):
@@ -64,5 +74,41 @@ class MinHeap:
 
         return min_item
 
+    def heappushpop(self, item):
+        if item < self.array[0]:
+            return item
+
+        min_item = self.array[0]
+        self.array[0] = item
+        self._bubble_down(0)
+        return min_item
+
+    def heapreplace(self, item):
+        min_item = self.array[0]
+        self.array[0] = item
+        self._bubble_down(0)
+        return min_item
+
     def get_min(self):
         return self.array[0]
+
+    @staticmethod
+    def heapsort(l):
+        h = MinHeap()
+        for i in l:
+            h.heappush(i)
+
+        sorted_l = []
+        while h:
+            sorted_l.append(h.heappop())
+
+        return sorted_l
+
+    @classmethod
+    def heapify(cls, l):
+        h = MinHeap()
+        h.array = l[:]
+        for i in range(len(h) // 2, -1, -1):
+            h._bubble_down(i)
+
+        return h
